680649db64c916443220665484e0cf8effcfd1d7
tayyab1068 <tayyabyousaf1431@gmail.com>
2026-08-20 01:51:59 +0500

    Refactor server.js for improved structure

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
-app.use(express.json());
 app.use(express.static("public"));
-
 io.on("connection", (socket) => {
  console.log("User connected:", socket.id);
- 
  socket.on("location-share", (data) => {
  console.log("Location received:", data);
  io.emit("location-update", {
@@ -19,16 +15,10 @@ io.on("connection", (socket) => {
  longitude: data.longitude
  });
  });
- 
  socket.on("disconnect", () => {
  console.log("User disconnected:", socket.id);
  });
 });
-
-app.get('/dashboard', (req, res) => {
- res.sendFile(__dirname + '/public/dashboard.html');
-});
-
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
  console.log(`Server running on port ${PORT}`);
